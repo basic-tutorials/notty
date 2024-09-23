27156accd7e96094032905e008754c1c29ba9014
Ismat <ismetsemedov@gmail.com>
2024-09-23 06:23:42 +0000

    backend

--- notty/notes/serializers.py
+++ notty/notes/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import Note
+from django.contrib.auth.models import User
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- notty/notes/views.py
+++ notty/notes/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
+from rest_framework import generics  
 from .models import Note
 from .serializers import NoteSerializer
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all().order_by('-created_at')
     serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated] 
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
